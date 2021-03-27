cb2a36a5cff29e0acb0637b4dd97a5490d9d2669
sakupuu <saku.yuzu.113@gmail.com>
2021-03-27 16:55:48 +0900

    編集済

--- test3.rb
+++ test3.rb
@@ -1,30 +1,30 @@
-def janken(player,computer)
-  if player == 0 && computer == 0
-    puts "あなたの手：#{hands[player]} 私の手：#{hands[computer]}"
+def janken(player,answers)
+  if player == 0 && answers == "グー"
+    puts "あなたの手：#{"グー"} 私の手：#{answers}"
     puts "あいこです"
-  elsif player == 1 && computer == 1
-    puts "あなたの手：#{hands[player]} 私の手：#{hands[computer]}"
+  elsif player == 1 && answers == "チョキ"
+    puts "あなたの手：#{"チョキ" << player} 私の手：#{answers}"
     puts "あいこです"
-  elsif player == 2 && computer == 2
-    puts "あなたの手：#{hands[player]} 私の手：#{hands[computer]}"
+  elsif player == 2 && answers == "パー"
+    puts "あなたの手：#{"パー" << player} 私の手：#{answers}"
     puts "あいこです"
-  elsif player == 1 && computer == 2
-    puts puts "あなたの手：#{hands[player]} 私の手：#{hands[computer]}"
+  elsif player == 1 && answers == "パー"
+    puts puts "あなたの手：#{"チョキ" << player} 私の手：#{answers}"
     puts "あなたの勝ちです"
-  elsif player == 0 && computer == 1
-    puts "あなたの手：#{hands[player]} 私の手：#{hands[computer]}"
+  elsif player == 0 && answers == "チョキ"
+    puts "あなたの手：#{"グー" << player} 私の手：#{answers}"
     puts "あなたの勝ちです"
-  elsif player == 2 && computer == 0
-    puts puts "あなたの手：#{hands[player]} 私の手：#{hands[computer]}"
+  elsif player == 2 && answers == "グー"
+    puts puts "あなたの手：#{"パー" << player} 私の手：#{answers}"
     puts "あなたの勝ちです"
-  elsif player == 1 && computer == 0
-    puts "あなたの手：#{hands[player]} 私の手：#{hands[computer]}"
+  elsif player == 1 && answers == "グー"
+    puts "あなたの手：#{"チョキ" << player} 私の手：#{answers}"
     puts "私の勝ちです"
-  elsif player == 0 && computer == 2  
-    puts "あなたの手：#{hands[player]} 私の手：#{hands[computer]}"
+  elsif player == 0 && answers == "パー"  
+    puts "あなたの手：#{"グー" << player} 私の手：#{answers}"
     puts "私の勝ちです"
-  elsif player == 2 && computer == 1
-    puts "あなたの手：#{hands[player]} 私の手：#{hands[computer]}"
+  elsif player == 2 && answers == "チョキ"
+    puts "あなたの手：#{"パー" << player} 私の手：#{answers}"
     puts "私の勝ちです"
   end
 end
@@ -36,53 +36,52 @@ puts "最初はグー、じゃんけん..."
 
 player = gets.to_i
 
-hands = ["グー", "チョキ", "パー"]
+answers = ["グー", "チョキ", "パー"]
 
+answer = answers.sample
 
-computer = hands.sample
+janken(player,answer)
 
-janken(player,computer)
-
-def janken2(player,computer)
+def janken2(player,answers)
   while player < 0 || player > 2 do
     puts "入力された値が無効です"
     puts "最初はグー、じゃんけん..."
     player = gets.to_i
 
-    if player == 0 && computer == 0
-      puts "あなたの手：#{hands[player]} 私の手：#{hands[computer]}"
+    if player == 0 && answers == "グー"
+      puts "あなたの手：#{"グー" << player} 私の手：#{answers}"
       puts "あいこです"
-    elsif player == 1 && computer == 1
-      puts "あなたの手：#{hands[player]} 私の手：#{hands[computer]}"
+    elsif player == 1 && answers == "チョキ"
+      puts "あなたの手：#{"チョキ" << player} 私の手：#{answers}"
       puts "あいこです"
-    elsif player == 2 && computer == 2
-      puts "あなたの手：#{hands[player]} 私の手：#{hands[computer]}"
+    elsif player == 2 && answers == "パー"
+      puts "あなたの手：#{"パー" << player} 私の手：#{answers}"
       puts "あいこです"
-    elsif player == 1 && compute == 2
-      puts puts "あなたの手：#{hands[player]} 私の手：#{hands[computer]}"
+    elsif player == 1 && answers == "パー"
+      puts puts "あなたの手：#{"チョキ" << player} 私の手：#{answers}"
       puts "あなたの勝ちです"
-    elsif player == 0 && computer == 1
-      puts "あなたの手：#{hands[player]} 私の手：#{hands[computer]}"
+    elsif player == 0 && answers == "チョキ"
+      puts "あなたの手：#{"グー" << player} 私の手：#{answers}"
       puts "あなたの勝ちです"
-    elsif player == 2 && computer == 0
-      puts puts "あなたの手：#{hands[player]} 私の手：#{hands[computer]}"
+    elsif player == 2 && answers == "グー"
+      puts puts "あなたの手：#{"パー" << player} 私の手：#{answers}"
       puts "あなたの勝ちです"
-    elsif player == 1 && computer == 0
-      puts "あなたの手：#{hands[player]} 私の手：#{hands[computer]}"
+    elsif player == 1 && answers == "グー"
+      puts "あなたの手：#{"チョキ" << player} 私の手：#{answers}"
       puts "私の勝ちです"
-    elsif player == 0 && computer == 2  
-      puts "あなたの手：#{hands[player]} 私の手：#{hands[computer]}"
+    elsif player == 0 && answers == "パー"  
+      puts "あなたの手：#{"グー" << player} 私の手：#{answers}"
       puts "私の勝ちです"
-    elsif player == 2 && computer == 1
-      puts "あなたの手：#{hands[player]} 私の手：#{hands[computer]}"
+    elsif player == 2 && answers == "チョキ"
+      puts "あなたの手：#{"パー" << player} 私の手：#{answers}"
       puts "私の勝ちです"
-  
+
     end
   end
 end
 
-janken2(player,computer)
+janken2(player,answer)
 
-hands = ["グー", "チョキ", "パー"]
+answers = ["グー", "チョキ", "パー"]
 
-computer = hands.sample
+answer = answers.sample
